e301fe133cea2d617690f4f3c8de239d922a3fbb
Joe Karasek <joseph.karasek@gmail.com>
2016-09-15 11:44:04 -0700

    fix heading fonts

--- wwwroot/js/custom.js
+++ wwwroot/js/custom.js
@@ -7,13 +7,14 @@
       $(this).html($(this).html().replace(/\s([^\s<]{0,10})\s*$/, '&nbsp;$1'));
     });
   };
-  HelloWorldDevs.prototype.mailForm = function (form, uid) {
+
+  HelloWorldDevs.prototype.mailForm = function (form, success_msg, uid) {
     var $form = $(form);
-    $form.before('<div class="form-error"></div>');
     $form.submit(function(e) {
       e.preventDefault();
       var formData = $form.serialize();
-      var formAction = 'http://web-api.tysonsteele.com/v1/webprops/'+uid+'/schedule';
+      var formAction = 'http://web-api.tysonsteele.com/v1/webprops/'+uid+'/schedule'
+      $('.form-error').remove();
       $.ajax({
         type: 'POST',
         url: formAction,
@@ -21,28 +22,29 @@
         dataType: 'json',
         encode: true
       }).done(function (response) {
-        $('.form-error').remove();
-        $form.replaceWith('Congratulations! Dentistry is a big part of a \
-	            healthy life, and we\'re excited to be a part of yours. We will \
-	            contact you in the next 2 business days to schedule your \
-	            appointment and to answer any questions you may still have. \
-	            Thank you!');
+        $form.replaceWith($(success_msg).html());
       }).error(function (response) {
-        var $error_list = $('<ul></ul>');
-        $.each(response.responseJSON, function(key, value) {
-          $error_list.append('<li>'+value+'</li>');
-        });
+        var $error_list = $('<ul>');
+        if(response.responseJSON == undefined) {
+          $error_list.append($('<li>').text('There was a problem with your submission. Please ensure all fields are correctly entered.'));
+        } else {
+          $.each(response.responseJSON, function(key, value) {
+            $error_list.append($('<li>').text(value));
+          });
+        }
+        $form.before('<div class="form-error"></div>');
         $('.form-error').html($error_list).fadeIn();
       });
     });
   };
+
   var HWD = new HelloWorldDevs();
   const $tourCarousel = $(".tour-carousel");
   const $tourModalCarousel = $('.tour-modal-carousel');
 
 
   HWD.noOrphans('h1,h2,h3,h4,h5,h6,li,p', '.price-box-h3-mid');
-  HWD.mailForm('#mail-form', '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Uid Goes Here!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+  HWD.mailForm('#mail-form', '#success_msg' , '7fb35345-752d-4792-9480-cd3db6674a62');
 
   // assign event handles to accordions
   $('.ui-accordion-header').click(function () {
